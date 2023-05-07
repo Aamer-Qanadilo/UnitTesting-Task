f2d570d61e1e3c4ec271d3068c2df1c1cb6f3936
Aamer-Qanadilo <aamer_qanadilo@hotmail.com>
2023-05-07 15:03:24 +0300

    Covered more cases and added the required features

--- calculator.js
+++ calculator.js
@@ -1,16 +1,24 @@
 const add = (nums = "") => {
   let sum = 0;
 
-  const numsArr = nums.split(",");
+  if (typeof nums !== "string")
+    throw new Error(
+      "Invalid Input Parameter. (Should pass a string of numbers separated by commas: '1,2').",
+    );
 
-  if (numsArr.length > 2) return 0;
+  const numsArr = nums.split(",");
+  const negatives = [];
 
   numsArr.forEach((number) => {
     const num = Number(number);
     if (!isNaN(num)) {
-      sum += num;
+      if (num > 0 && num <= 1000) sum += num;
+      if (num < 0) negatives.push(num);
     }
   });
+
+  if (negatives.length)
+    throw new Error(`negatives not allowed [${negatives.join(", ")}]`);
   return sum;
 };
 
